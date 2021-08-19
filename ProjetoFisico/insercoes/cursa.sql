f22df9791af7bbb3f40050303079518cde7b39c5
Maria Luísa <mlss@cin.ufpe.br>
2021-08-19 11:18:10 -0300

    ajustando DE NOVO as insercoes

--- ProjetoFisico/insercoes/cursa.sql
+++ ProjetoFisico/insercoes/cursa.sql
@@ -48,114 +48,114 @@ VALUES ('NT','DCAT', date'1994-09-10');
 
 -- feitiços
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('VG','FT', datedate'1993-09-01');
+VALUES ('VG','FT', date'1993-09-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('GG','FT', datedate'1993-09-01');
+VALUES ('GG','FT', date'1993-09-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('LL','FT', datedate'1993-09-01');
+VALUES ('LL','FT', date'1993-09-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('CC','FT', datedate'1993-09-01');
+VALUES ('CC','FT', date'1993-09-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('DM','FT', datedate'1993-09-01');
+VALUES ('DM','FT', date'1993-09-01');
 
 -- herbologia 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('VG','HB', datedate'1994-09-01');
+VALUES ('VG','HB', date'1994-09-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('GG','HB', datedate'1994-09-01');
+VALUES ('GG','HB', date'1994-09-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('CC','HB', datedate'1994-09-01');
+VALUES ('CC','HB', date'1994-09-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('DM','HB', datedate'1994-09-01');
+VALUES ('DM','HB', date'1994-09-01');
 
 -- historia
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('LL','HM', datedate'1993-09-01');
+VALUES ('LL','HM', date'1993-09-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('GG','HM', datedate'1993-09-01');
+VALUES ('GG','HM', date'1993-09-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('DM','HM', datedate'1993-09-01');
+VALUES ('DM','HM', date'1993-09-01');
 
 -- defesa contra artes das trevas
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('VG','DCAT', datedate'1994-09-10');
+VALUES ('VG','DCAT', date'1994-09-10');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('GG','DCAT', datedate'1994-09-10');
+VALUES ('GG','DCAT', date'1994-09-10');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('LL','DCAT', datedate'1994-09-10');
+VALUES ('LL','DCAT', date'1994-09-10');
 
 -- Alquimia
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('VG','AL', datedate'1994-09-10');
+VALUES ('VG','AL', date'1994-09-10');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('GG','AL', datedate'1994-09-10');
+VALUES ('GG','AL', date'1994-09-10');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('CC','AL', datedate'1994-09-10');
+VALUES ('CC','AL', date'1994-09-10');
 
 -- Poções
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('LL','PO', datedate'1993-09-01');
+VALUES ('LL','PO', date'1993-09-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('GG','PO', datedate'1993-09-01');
+VALUES ('GG','PO', date'1993-09-01');
 
 -- Estudos Antigos
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('LL','EA', datedate'1993-04-01');
+VALUES ('LL','EA', date'1993-04-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('CC','EA', datedate'1993-04-01');
+VALUES ('CC','EA', date'1993-04-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('DM','EA', datedate'1993-04-01');
+VALUES ('DM','EA', date'1993-04-01');
 
 -- Transfiguração
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('GG','TR', datedate'1992-04-01');
+VALUES ('GG','TR', date'1992-04-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('CC','TR', datedate'1992-04-01');
+VALUES ('CC','TR', date'1992-04-01');
 
 -- Trato das criaturas
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('GG','TCM', datedate'1993-04-01');
+VALUES ('GG','TCM', date'1993-04-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('DM','TCM', datedate'1993-04-01');
+VALUES ('DM','TCM', date'1993-04-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('CC','TCM', datedate'1993-04-01');
+VALUES ('CC','TCM', date'1993-04-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('VG','TCM', datedate'1993-04-01');
+VALUES ('VG','TCM', date'1993-04-01');
 
 -- Teoria da magia
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('GG','TM', datedate'1993-09-01');
+VALUES ('GG','TM', date'1993-09-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('LL','TM', datedate'1993-09-01');
+VALUES ('LL','TM', date'1993-09-01');
 
 INSERT INTO cursa(varinha, cod_disciplina, dt_inicio) 
-VALUES ('CC','TM', datedate'1993-09-01');
+VALUES ('CC','TM', date'1993-09-01');
